6ed75049b0d58988ec56153b99e9b00c75bdac63
Alexandre Boyer <alexandre.boyer@kedgebs.com>
2017-11-10 12:19:29 +0100

    Add figaro - Protect my secret data in config/application.yml

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,21 @@
 class DosesController < ApplicationController
 
- def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  before_action :set_cocktail, only: [:create]
 
   def create
-    @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    if @dose.save
-      redirect_to cocktail_path(@dose.cocktail)
+   @dose = @cocktail.doses.new(dose_params)
+
+    if @dose.save!
+      redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render "cocktails/show"
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-
-    redirect_to dose_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
@@ -27,5 +23,8 @@ class DosesController < ApplicationController
   def dose_params
     params.require(:dose).permit(:description,:ingredient_id)
   end
-end
 
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ingredients = Ingredient.create ([{ name: "Sugar"}, { name: "Mint"}, { name: "Ron"}, { name: "Whisky"},
+  { name: "Martini"}, { name: "Vodka"}, { name: "Orange juice"}, { name: "Lemon Juice"}, { name: "Ice"},
+  { name: "Lemonade"}, { name: "Pineapple juice"}, { name: "Coca-Cola"}, { name: "Gin"} ])
